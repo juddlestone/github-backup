ece706d4ecc1af60154733c1c4e85515b993f03b
Jack Huddlestone <jack.huddlestone@littlefish.co.uk>
2025-12-11 12:52:07 +0000

    feat(github-backup): enhance backup workflow and script

--- scripts/backup.ps1
+++ scripts/backup.ps1
@@ -1,40 +1,57 @@
 #! /bin/pwsh
+function Backup-GithubRepositories {
+    Param (
+        [Parameter(Mandatory=$true)]
+        [string]$StorageAccountName
+    )
 
-# gh auth --with-token
 
-$backupPath = Join-Path -Path (Get-Location) -ChildPath "backups"
-$backupArchivePath = Join-Path -Path (Get-Location) -ChildPath "backup-archives"
-$backupPaths = @($backupPath, $backupArchivePath)
+    # Variables
+    $backupPath = Join-Path -Path (Get-Location) -ChildPath "backups"
+    $backupArchivePath = Join-Path -Path (Get-Location) -ChildPath "backup-archives"
+    $backupPaths = @($backupPath, $backupArchivePath)
+    $date = Get-Date -Format "yyyy-MM-dd"
 
 
-$backupPaths | ForEach-Object {
-    if (-Not (Test-Path -Path $_)) {
-        try { 
-            New-Item -ItemType Directory -Path $_ -ErrorAction Stop | Out-Null
-            Write-Output "Created directory: $_"
-        } catch {
-            Write-Error "Failed to create directory $_`: $_"
+    # Ensure backup directories exist, create them if they don't
+    $backupPaths | ForEach-Object {
+        if (-Not (Test-Path -Path $_)) {
+            try { 
+                New-Item -ItemType Directory -Path $_ -ErrorAction Stop | Out-Null
+                Write-Output "Created directory: $_"
+            } catch {
+                Write-Error "Failed to create directory $_`: $_"
+            }
         }
     }
-}
 
 
-gh repo list --json url | ConvertFrom-Json | ForEach-Object {
-    $repository = $_.url
-    $repositoryName = [System.IO.Path]::GetFileNameWithoutExtension($repository)
-    $repositoryPath = Join-Path -Path $backupPath -ChildPath $repositoryName
-
-    Write-Output "Cloning Repo: '$repositoryName'"
-    git clone $repository $repositoryPath | Out-Null
-    Compress-Archive -Path $repositoryPath -DestinationPath "$backupArchivePath/$repositoryName.zip" -Force
-}
+    # Create blob container for these dates
+    $storageContext = New-AzStorageContext -StorageAccountName $StorageAccountName -UseConnectedAccount
+    $containerName = New-AzStorageContainer -Name $date -Context $storageContext -Permission Off
 
 
-$backupPaths | ForEach-Object {
-    try { 
-        Remove-Item -Path $_ -Recurse -ErrorAction Stop -Force
-        Write-Output "Deleted directory: $_"
-    } catch {
-        Write-Error "Failed to delete directory : $_"
+    # Fetch list of repositories and back them up
+    gh repo list --json url | ConvertFrom-Json | ForEach-Object {
+        $repository = $_.url
+        $repositoryName = [System.IO.Path]::GetFileNameWithoutExtension($repository)
+        $repositoryPath = Join-Path -Path $backupPath -ChildPath $repositoryName
+        $destinationPath = "$backupArchivePath/$repositoryName.zip"
+
+        Write-Output  "Backing up repository: $repositoryName"
+        git clone $repository $repositoryPath
+        Compress-Archive -Path $repositoryPath -DestinationPath $destinationPath -Force
+
+
+    }
+
+
+    $backupPaths | ForEach-Object {
+        try { 
+            Remove-Item -Path $_ -Recurse -ErrorAction Stop -Force
+            Write-Output "Deleted temporary directory: $_"
+        } catch {
+            Write-Error "Failed to delete directory : $_"
+        }
     }
 }
